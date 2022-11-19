d0480d8750c6adc4682b8dd9ae0a9bf76f8ddc03
MALIHA022 <114968869+MALIHA022@users.noreply.github.com>
2022-11-19 11:56:18 +0600

    Create M.py
    Print "M"

---
+++ M.py
@@ -0,0 +1,16 @@
+'''M'''
+
+
+size = int(input("Enter a size: "))
+num = size
+
+for row in range(1,num+1):
+    for col in range(1,num+1):
+        trick = row+col
+        if col==1 or col==num or (col==row and row<((num+1)//2)) or (trick==(num+1) and row<=((num+1)//2)):
+            sign = "*"
+            print(sign,end="")
+        else:
+            sign = " "
+            print(sign,end="")
+    print()    
