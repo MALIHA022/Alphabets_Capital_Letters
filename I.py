e56ff5ba7c83f0fbe11d3ae498b6f35bfb407dd5
MALIHA022 <114968869+MALIHA022@users.noreply.github.com>
2022-11-19 11:14:38 +0600

    Create I.py
    Print "I"

---
+++ I.py
@@ -0,0 +1,15 @@
+'''I'''
+
+size = int(input("Enter the size of your alphabet: "))
+num = size
+
+for row in range(1,num+1):
+    for col in range(1,num+1):
+        if row == 1 or row == num or col == ((num+1)//2):
+            sign = "*"
+            
+        else:
+            sign = " "
+        print(sign, end="")
+        
+    print()
