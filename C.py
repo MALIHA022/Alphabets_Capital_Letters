0f395aec619cf505c38256ff4f015c802a690a0c
MALIHA022 <114968869+MALIHA022@users.noreply.github.com>
2022-11-19 11:09:58 +0600

    Create C.py
    Print "C"

---
+++ C.py
@@ -0,0 +1,17 @@
+'''C'''
+size = int(input("Enter a number: "))
+num = size
+
+for row in range(1,num+1):
+    for col in range(1,num+1):
+        
+        if row == 1 or col == 1 or row ==num or (row == 2 and col == num) or (row == (num-1) and col==num):
+            sign = "*"
+            if (row == (num+1)//2 and col ==num) or (row == 1 and col== num) or (row == num and col == num) or (row==1 and col==1) or (row==num and col==1): 
+               sign = " "
+          
+            
+            print(sign, end="")
+        else:
+            print(" ",end="")
+    print()
