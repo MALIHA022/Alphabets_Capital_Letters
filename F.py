ef9582b24a4b7f28123cfb276416673c637364ef
MALIHA022 <114968869+MALIHA022@users.noreply.github.com>
2022-11-19 11:12:15 +0600

    Create F.py
    Print "F"

---
+++ F.py
@@ -0,0 +1,18 @@
+'''F'''
+size = int(input("Enter a number: "))
+num = size
+
+for row in range(1,num+1):
+    for col in range(1,num+1):
+        
+        value = (num+1)//2
+        if (row == 1 ) or col == 1 or row==var :
+            if size%2 == 0:
+                var = value +1  
+                row == var 
+            else:
+                var = value
+                row == var
+            print("*" , end="")
+    print()
+    
